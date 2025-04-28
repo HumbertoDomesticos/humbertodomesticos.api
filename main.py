e8cc35333a4ac19aaec292101b571d2767e0da4e
AlexiaCazale <alexiacazale7@gmail.com>
2025-04-27 23:12:26 -0300

    feat: conexão com o front

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, requests
 import uvicorn
 from classes.controllers import ProdutoController, AccessController
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # libera para seu frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # permite todos os métodos HTTP
+    allow_headers=["*"],  # permite todos os headers
+)
 
 access_controller = AccessController()
 produto_controller = ProdutoController()
--- rotas/check_bd.py
+++ rotas/check_bd.py
@@ -2,4 +2,5 @@ from main import app, access_controller
 
 @app.get('/is-connected')
 def is_connected():
-    return {"dado": access_controller.teste_query()}
+    # return {"dado": access_controller.teste_query()}
+    return access_controller.teste_query2()
