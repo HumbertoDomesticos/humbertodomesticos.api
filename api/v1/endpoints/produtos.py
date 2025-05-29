3c6e8332979f9d02df129bf6403097079e837ed5
Lallindo <brunolallo8@gmail.com@>
2025-05-29 13:46:02 -0300

    feat: Endpoint para pegar produto por id

--- api/v1/endpoints/produtos.py
+++ api/v1/endpoints/produtos.py
@@ -1,21 +1,21 @@
 from fastapi import APIRouter, Path, Query, HTTPException
 from sqlalchemy import text
 from typing import List, Annotated
-from models.db_models import ProdutosDB, em_categoria as cat_table
-from models.schemas import produtos, em_categoria, categorias
+from models.db_models import ProdutosDB, em_categoria as cat_table, ImagesDB
+from models.schemas import produtos, em_categoria, categorias, images
 from main import db_dependency
 
 router = APIRouter(prefix='/produtos', tags=['produtos'])
 
 @router.get('/')
-async def buscar_produto(
+async def get_produtos(
     db: db_dependency
 ) -> List[produtos.ProdutosResponse]|produtos.ProdutosResponse:
     db_produto = db.query(ProdutosDB)
     return db_produto
 
 @router.post('/')
-async def criar_produto(
+async def post_produtos(
     produto: produtos.ProdutosCreate, 
     db: db_dependency
 ):
@@ -24,7 +24,7 @@ async def criar_produto(
     db.commit()
 
 @router.patch('/')
-async def alterar_produto(
+async def update_produto(
     id_prod: Annotated[int, Query(description="Id do produto")],
     produto: produtos.ProdutosPatch,
     db: db_dependency 
@@ -32,8 +32,16 @@ async def alterar_produto(
     print(produto.model_dump(exclude_defaults=True))
     return
 
+@router.get('/{id_prod}')
+async def get_produto(
+    id_prod: Annotated[int, Path(description="Id do produto")],
+    db: db_dependency
+) -> produtos.ProdutosResponse:
+    stmt = db.get(ProdutosDB, id_prod)
+    return stmt
+
 @router.post('/add-categoria')
-async def insert_em_cat(
+async def insert_categoria_produto(
     em_cat: em_categoria.EmCategoriaCreate,
     db: db_dependency
 ):
